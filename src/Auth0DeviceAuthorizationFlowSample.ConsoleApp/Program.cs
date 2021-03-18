45e00c9fcc260cf19aa792aa5917c9d4fa726884
Adam Smith <adam@rxindustries.io>
2021-03-18 17:19:35 +0000

    Baseline checkin

---
+++ src/Auth0DeviceAuthorizationFlowSample.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auth0DeviceAuthorizationFlowSample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
